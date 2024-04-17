e7faed11723033c806bc555d2d13635dc286e8f8
Adrian Alcocer <jadrianaldo@outlook.com>
2024-04-16 19:14:52 -0600

    Finalizada Pestaña de inicio con darkmode y responsive

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,10 @@
 import darkTheme from "./dark_mode.js";
 
 darkTheme(".dark-theme-btn", "dark-mode");
+
+const menu = document.querySelector(".menu");
+const btn = document.querySelector(".burguer");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("active");
+});
